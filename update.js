cb73f4dfae16cf1eaef8a3baba7c4addb10db5b1
hoangthanh751996 <hoangthanh751996@gmail.com>
2018-04-23 09:45:29 +0700

    okmen

--- update.js
+++ update.js
@@ -3,57 +3,57 @@ const config = require('config');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 
-let updateStatePage = async () => {
-    const db = await MongoClient.connect(url);
-    const dbo = await  db.db("adsbold-page");
-    const pages = await dbo.collection("pages").find({is_crawling: true}).toArray();
-    if(pages.length === 0 ){
-
+let updateStatePage = (async () => {
+  const db = await MongoClient.connect(url);
+  const dbo = await  db.db("adsbold-page");
+  const pages = await dbo.collection("pages").find({is_crawling: true}).toArray();
+  if (pages.length === 0) {
+    console.log("Don't have pages be crawling");
+  } else {
+    let page_fb_ids = await getQueueActive();
+    if (page_fb_ids.length === 0) {
+      const query = {is_crawling: true};
+      const diff = {$set: {is_crawling: false}};
+      await dbo.collection("pages").updateMany(query, diff);
+      console.log("UDPATE SUCCESS");
     } else {
-        let page_fb_ids = await getQueueActive();
-        if(page_fb_ids.length === 0) {
-            const query = { is_crawling: true};
-            const diff = { $set: {is_crawling: false} };
-            await dbo.collection("pages").updateMany(query, diff);
-            console.log("UDPATE SUCCESS");
-        } else {
-            const pages = await dbo.collection("pages").find({is_crawling: true}).toArray();
-            const pagesCrawling = pages.map(e => e.fb_id);
-            const filterPages = pagesCrawling.filter(function(e) {
-                return page_fb_ids.indexOf(e) === -1;
-            });
-            for(const page of filterPages) {
-                const query = { fb_id: page};
-                const diff = { $set: {is_crawling: false} };
-                await dbo.collection("pages").updateOne(query, diff);
-                console.log("UDPATE SUCCESS");
-            }
-        }
+      const pages = await dbo.collection("pages").find({is_crawling: true}).toArray();
+      const pagesCrawling = pages.map(e => e.fb_id);
+      const filterPages = pagesCrawling.filter(function (e) {
+        return page_fb_ids.indexOf(e) === -1;
+      });
+      for (const page of filterPages) {
+        const query = {fb_id: page};
+        const diff = {$set: {is_crawling: false}};
+        await dbo.collection("pages").updateOne(query, diff);
+        console.log("UDPATE SUCCESS");
+      }
     }
-    await db.close();
-    process.exit(0);
-};
+  }
+  await db.close();
+  process.exit(0);
+})();
 
 let getQueueActive = () => {
-    let page_fb_ids = [];
-    return new Promise(resolve => {
-        kue.createQueue().active(async function( err, ids ) {
-            if(err) return cb(true, err);
-            for(const id of ids) {
-                let title = await getJob(id);
-                let str = title.split(" ");
-                page_fb_ids = page_fb_ids.concat([str[str.length - 1]]);
-            }
-            return resolve(page_fb_ids);
-        });
-    })
+  let page_fb_ids = [];
+  return new Promise(resolve => {
+    kue.createQueue().active(async function (err, ids) {
+      if (err) return cb(true, err);
+      for (const id of ids) {
+        let title = await getJob(id);
+        let str = title.split(" ");
+        page_fb_ids = page_fb_ids.concat([str[str.length - 1]]);
+      }
+      return resolve(page_fb_ids);
+    });
+  })
 };
 
 let getJob = (id) => {
-    return new Promise(resolve => {
-        kue.Job.get( id,function( err, job ) {
-            return resolve(job ? job.data.title : null);
-        });
-    })
+  return new Promise(resolve => {
+    kue.Job.get(id, function (err, job) {
+      return resolve(job ? job.data.title : null);
+    });
+  })
 };
 
